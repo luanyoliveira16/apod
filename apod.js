76b5713773573c7741deb7d7eb4bc50fb745781d
Luany Oliveira <luanyoliveira16@gmail.com>
2022-01-30 16:13:04 -0300

    trabalhando no js

---
+++ apod.js
@@ -0,0 +1,13 @@
+$("#submit").click(function () {
+    let data = $("#data").val();
+    $.ajax({
+      url: `https://api.nasa.gov/planetary/apod?api_key=b2e4yQbTO2NueoODTlnAaA3tb7mui6cQpTpSLI6I&date=${data}`,
+      type: "GET",
+      success: function (apod) {
+        $("#principal").css("display", "flex");
+        $("#imageofday").attr("src", apod.url);
+        $("#textofday").text(apod.title);
+        $("#explanation").text(apod.explanation);
+      },
+    });
+  });
